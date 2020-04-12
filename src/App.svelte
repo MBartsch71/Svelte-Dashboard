559da0adf7bc991b17bc399db0ac9abcc5d39249
Matthias Bartsch <bartschmatthias@mac.com>
2020-04-12 22:58:44 +0200

    Chart.js begin

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,18 @@
 <script>
-
+  import Chart from "./Chart.svelte";
 </script>
+
+<style>
+  .chart {
+    padding: 0 1rem;
+    width: 50%;
+    height: 50%;
+  }
+</style>
 
 <div>
   <h1>CQA Dashboard</h1>
+  <p class="chart">
+    <Chart />
+  </p>
 </div>
